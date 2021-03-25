70337bb7fffc790c9e8d466349c17e0206346dfb
wonh <xyangtuo@163.com>
2021-03-26 02:37:37 +0800

    fix td3

--- repetition/td3/td3_core.py
+++ repetition/td3/td3_core.py
@@ -7,15 +7,16 @@ def mlp(sizes, activation, output_activation=nn.Identity):
     layers = []
     for i in range(len(sizes)-1):
         act = activation if i < len(sizes)-2 else output_activation
-        layers.append(nn.Linear(sizes[i], sizes[i+1]))
-        layers.append(act())
+        # layers.append(nn.Linear(sizes[i], sizes[i+1]))
+        # layers.append(act())
+        layers += [nn.Linear(sizes[i], sizes[i+1]), act()]
     return nn.Sequential(*layers)
 
 class Actor(nn.Module):
 
     def __init__(self, obs_dim, act_dim, limit, hid_size, activation):
         super().__init__()
-        self.model = mlp([obs_dim]+list(hid_size)+[act_dim], activation)
+        self.model = mlp([obs_dim]+list(hid_size)+[act_dim], nn.Tanh)
         self.act_limit = limit
 
     def forward(self, obs):
@@ -32,19 +33,18 @@ class Critic(nn.Module):
 
 class Actor_Critic(nn.Module):
 
-    def __init__(self, obs_space, act_space, hid_size=(256,256), activation=nn.Tanh):
+    def __init__(self, obs_space, act_space, hid_size=(256,256), activation=nn.ReLU):
         super().__init__()
         if isinstance(act_space, Discrete):
             raise TypeError
         else:
             obs_dim = obs_space.shape[0]
             act_dim = act_space.shape[0]
-        self.limit = act_space.high[0]
+        act_limit = act_space.high[0]
         self.q = Critic(obs_dim, act_dim, hid_size, activation)
         self.q2 = Critic(obs_dim, act_dim, hid_size, activation)
-        self.pi = Actor(obs_dim, act_dim, self.limit, hid_size, activation)
+        self.pi = Actor(obs_dim, act_dim, act_limit, hid_size, activation)
 
     def act(self, obs):
         with torch.no_grad():
-            res = self.pi(obs)
-            return res.numpy()
+            return self.pi(obs).numpy()
--- repetition/td3/td3_main.py
+++ repetition/td3/td3_main.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 from gym.spaces import Discrete
 import argparse
 import gym
+import itertools
 
 class Buffer(object):
 
@@ -17,10 +18,10 @@ class Buffer(object):
         self.rew_buf = np.zeros((size,), dtype=np.float32)
         self.obs2_buf = np.zeros((size, obs_dim), dtype=np.float32)
         self.done_buf = np.zeros((size, ), dtype=np.float32)
-        self.ptr, self.size = 0, size
+        self.ptr, self.size = 0, 0
 
     def store(self, obs, act, rew, obs2, done):
-        assert self.ptr < self.size
+        # assert self.ptr < self.size
         self.obs_buf[self.ptr] = obs
         self.act_buf[self.ptr] = act
         self.rew_buf[self.ptr] = rew
@@ -29,15 +30,19 @@ class Buffer(object):
         self.ptr = (self.ptr + 1) % self.max_size
         self.size = min(self.size + 1, self.max_size)
 
+
     def sample_batch(self, batch_size=32):
+        # print(self.obs_buf[:10])
         idx = np.random.randint(0, self.size, batch_size)
         data = dict(s=self.obs_buf[idx], a=self.act_buf[idx], r=self.rew_buf[idx], s2=self.obs2_buf[idx], d=self.done_buf[idx])
-
         return {k:torch.as_tensor(v, dtype=torch.float32) for k, v in data.items()}
 
 
 def td3(env_fn, epochs, steps_per_epoch, max_ep_len, start_steps, buffer_size, batch_size, q_lr, pi_lr,
-        update_every, update_after, polyak=0.995, gamma=0.99, act_noise=0.1, sigma=0.1):
+        update_every, update_after, polyak=0.995, gamma=0.99, act_noise=0.1, noise_clip=0.5, target_noise=0.2):
+    seed = 0
+    torch.manual_seed(seed)
+    np.random.seed(seed)
 
     env = env_fn()
     if isinstance(env.action_space, Discrete):
@@ -45,8 +50,8 @@ def td3(env_fn, epochs, steps_per_epoch, max_ep_len, start_steps, buffer_size, b
     else:
         act_dim = env.action_space.shape[0]
     obs_dim, act_dim = env.observation_space.shape[0], env.action_space.shape[0]
-    limit = env.action_space.high[0]
-    clip = 0.5
+    act_limit = env.action_space.high[0]
+    # print(obs_dim, act_dim)
 
     ac = Actor_Critic(env.observation_space, env.action_space)
     ac_target = deepcopy(ac)
@@ -55,35 +60,24 @@ def td3(env_fn, epochs, steps_per_epoch, max_ep_len, start_steps, buffer_size, b
 
     buffer = Buffer(obs_dim, act_dim, buffer_size)
 
-    q1_optimizer = torch.optim.Adam(ac.q.parameters(), lr=q_lr)
-    q2_optimizer = torch.optim.Adam(ac.q2.parameters(), lr=q_lr)
+    q_param = itertools.chain(ac.q.parameters(), ac.q2.parameters())
+    q_optimizer = torch.optim.Adam(q_param, lr=q_lr)
     pi_optimizer = torch.optim.Adam(ac.pi.parameters(), lr=pi_lr)
-
-    # def compute_q_loss(data):
-    #
-    #     return q_loss_1, q_loss_2
-
-    def compute_pi_loss(data):
-        s, a, r, s2, d = data['s'], data['a'], data['r'], data['s2'], data['d']
-        for params in ac.q.parameters():
-            params.require_grad = False
-        loss = (ac.q(s, ac.pi(s))).mean()
-        return loss
 
     def update_q(data):
         s, a, r, s2, d = data['s'], data['a'], data['r'], data['s2'], data['d']
-        eplison = np.random.normal(0, sigma)
-        a2 = np.clip(ac_target.act(s2) + np.clip(eplison, -clip, clip), -limit, limit)
-        a2 = torch.as_tensor(a2, dtype=torch.float32)
-        # print(ac_target.q(s2, a2).shape)
         with torch.no_grad():
+            pi_target = ac_target.pi(s2)
+            eplison = torch.rand_like(pi_target) * target_noise
+            eplison = torch.clamp(eplison, -noise_clip, noise_clip)
+            a2 = torch.clamp(eplison + pi_target, -act_limit, act_limit)
             y = r + gamma * (1 - d) * torch.min(ac_target.q(s2, a2), ac_target.q2(s2, a2))
-        q1_optimizer.zero_grad()
-        q_loss = -(ac.q(s, a) - y).mean()
+
+        q_optimizer.zero_grad()
+        q_loss = ((ac.q(s, a) - y)**2).mean() + ((ac.q2(s, a) - y)**2).mean()
         q_loss.backward()
-        q2_optimizer.zero_grad()
-        q2_loss = -(ac.q2(s, a) - y).mean()
-        q2_loss.backward()
+        q_optimizer.step()
+
 
     def update_target(model, tar_model):
         for para, tar_para in zip(model.parameters(), tar_model.parameters()):
@@ -92,19 +86,26 @@ def td3(env_fn, epochs, steps_per_epoch, max_ep_len, start_steps, buffer_size, b
 
     def update_pi_tar(data):
         # update policy
+        for q in q_param:
+            q.require_grad = False
         pi_optimizer.zero_grad()
-        loss_pi = compute_pi_loss(data)
+        s = data['s']
+        q_pi = ac.q(s, ac.pi(s))
+        loss_pi = -q_pi.mean()
         loss_pi.backward()
         pi_optimizer.step()
+        for q in q_param:
+            q.require_grad = True
         # update target
-        update_target(ac.q, ac_target.q)
-        update_target(ac.q2, ac_target.q2)
-        update_target(ac.pi, ac_target.pi)
+        with torch.no_grad():
+            update_target(ac.q, ac_target.q)
+            update_target(ac.q2, ac_target.q2)
+            update_target(ac.pi, ac_target.pi)
 
     def get_action(obs, act_noise):
         act = ac.act(torch.as_tensor(obs, dtype=torch.float32))
         act += act_noise * np.random.randn(act_dim)
-        return np.clip(act, -limit, limit)
+        return np.clip(act, -act_limit, act_limit)
 
     total_steps = epochs * steps_per_epoch
 
@@ -128,24 +129,25 @@ def td3(env_fn, epochs, steps_per_epoch, max_ep_len, start_steps, buffer_size, b
             obs, ep_len, ep_ret = env.reset(), 0, 0
 
         if t > update_after and t % update_every==0:
-            data = buffer.sample_batch(batch_size)
-            update_q(data)
-            if t % update_every*2 == 0:
-                update_pi_tar(data)
+            for i in range(update_every):
+                data = buffer.sample_batch(batch_size)
+                update_q(data)
+                if i % 2 ==0:
+                    update_pi_tar(data)
 
 # if __name__ == '__main__':
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--env", type=str, default='CartPole-v0')
-    parser.add_argument("--epochs", type=int, default=40)
+    parser.add_argument("--epochs", type=int, default=100)
     parser.add_argument("--steps_per_epoch", type=int, default=4000)
-    parser.add_argument("--start_steps", type=int, default=10000)
+    parser.add_argument("--start_steps", type=int, default=50000)
     parser.add_argument("--max_ep_len", type=int, default=1000)
-    parser.add_argument("--buffer_size", type=int, default=10000)
+    parser.add_argument("--buffer_size", type=int, default=1000000)
     parser.add_argument("--update_after", type=int, default=1000)
-    parser.add_argument("--update_every", type=int, default=10000)
-    parser.add_argument("--batch_size", type=int, default=10000)
+    parser.add_argument("--update_every", type=int, default=50)
+    parser.add_argument("--batch_size", type=int, default=100)
     args = parser.parse_args()
 
     td3(lambda: gym.envs.make(args.env), args.epochs, args.steps_per_epoch, args.max_ep_len, args.start_steps, args.buffer_size, args.batch_size, 1e-3, 1e-3,
-            args.update_every, args.update_after, polyak=0.995, gamma=0.99, act_noise=0.1, sigma=0.1)
+            args.update_every, args.update_after, polyak=0.995, gamma=0.99, act_noise=0.1, )
